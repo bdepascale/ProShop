83177edeefe7d7d5007b7c9d06a7ab285a8392b9
Brian DePascale <bdepascale@gmail.com>
2021-01-31 08:50:44 -0500

    added Header, Footer, Bootstrap components, fontawesome
    slight adjustments to page layout, added containers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 
-
-const App=()=> {
+const App = () => {
   return (
     <>
-     <h1>Welcome to ProShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
